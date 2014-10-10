192f1cd861dd2744445478b767809162792c8b65
LiBinggen <cralison@163.com>
2014-10-11 05:29:41 +0800

    Initial Commit

---
+++ BGDouBanMusic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BGDouBanMusic
+//
+//  Created by LiBinggen on 14-10-11.
+//  Copyright (c) 2014年 Libinggen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BGDBMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BGDBMAppDelegate class]));
+    }
+}
